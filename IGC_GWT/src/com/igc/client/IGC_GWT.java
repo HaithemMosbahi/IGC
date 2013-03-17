be4ff8f9d6afd5a25c3a50b014ae4e549fe2c8df
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2013-03-17 13:10:41 +0100

    First Commit

---
+++ IGC_GWT/src/com/igc/client/IGC_GWT.java
@@ -0,0 +1,19 @@
+package com.igc.client;
+
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class IGC_GWT implements EntryPoint {
+	
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
